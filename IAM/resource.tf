b34d53e4d7d38c304541662ca4bf43556a5c3d6a
suzhetao <suzhetao@gmail.com>
2024-12-17 17:13:47 +0800

    deploy

--- IAM/resource.tf
+++ IAM/resource.tf
@@ -19,6 +19,7 @@ module "alicloud_ram_role" {
 module "alicloud_resource_manager_control_policy" {
   source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/control-policy?ref=1.x"
   alicloud_resources = var.alicloud_resources
+  depends_on         = [module.alicloud_resource_manager_resource_directory]
 }
 
 # 启用资源目录
@@ -45,9 +46,9 @@ module "alicloud_resource_manager_control_policy_attachment" {
 }
 
 # 成员
-module "alicloud_resource_manager_account" {
-  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/account?ref=1.x"
-  alicloud_resources = var.alicloud_resources
-  tags               = var.tags
-  depends_on         = [module.alicloud_resource_manager_folder]
-}
+#module "alicloud_resource_manager_account" {
+#  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/account?ref=1.x"
+#  alicloud_resources = var.alicloud_resources
+#  tags               = var.tags
+#  depends_on         = [module.alicloud_resource_manager_folder]
+#}
--- terragrunt.hcl
+++ terragrunt.hcl
@@ -17,7 +17,18 @@ terraform {
       version = "3.6.3"
     }
   }
-  backend "http" {
+  backend "s3" {
+    bucket = "tfstate"
+    key    = "tfstate/terragrunt-gitlab-alicloud/${path_relative_to_include()}/terraform.tfstate"
+    region = "us-east-1"
+    endpoints = {
+      s3 = "http://obs.home.local"
+    }
+    skip_credentials_validation = true
+    skip_requesting_account_id = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    use_path_style = true   
   }
 }
 
