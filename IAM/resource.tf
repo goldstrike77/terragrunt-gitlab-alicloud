bc8216195a58222b0a7734394123dc94d64fa602
suzhetao <suzhetao@gmail.com>
2024-12-20 15:23:29 +0800

    deploy

--- IAM/resource.tf
+++ IAM/resource.tf
@@ -44,21 +44,21 @@ module "alicloud_resource_manager_control_policy" {
 }
 
 # 资源夹
-module "alicloud_resource_manager_folder" {
-  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/folder?ref=1.x"
-  alicloud_resources = var.alicloud_resources
-  depends_on         = [module.alicloud_resource_manager_resource_directory]
-}
+#module "alicloud_resource_manager_folder" {
+#  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/folder?ref=1.x"
+#  alicloud_resources = var.alicloud_resources
+#  depends_on         = [module.alicloud_resource_manager_resource_directory]
+#}
 
 # 管控策略绑定
-module "alicloud_resource_manager_control_policy_attachment" {
-  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/control-policy-attachment?ref=1.x"
-  alicloud_resources = var.alicloud_resources
-  depends_on = [
-    module.alicloud_resource_manager_folder,
-    module.alicloud_resource_manager_control_policy
-  ]
-}
+#module "alicloud_resource_manager_control_policy_attachment" {
+#  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/control-policy-attachment?ref=1.x"
+#  alicloud_resources = var.alicloud_resources
+#  depends_on = [
+#    module.alicloud_resource_manager_folder,
+#    module.alicloud_resource_manager_control_policy
+#  ]
+#}
 
 # 成员
 #module "alicloud_resource_manager_account" {
