76f20c3d144ce1b7515c05bebfa2d0c1d1a6d910
suzhetao <suzhetao@gmail.com>
2024-12-23 10:41:12 +0800

    deploy

--- IAM/resource.tf
+++ IAM/resource.tf
@@ -31,10 +31,10 @@ module "alicloud_ram_group" {
 }
 
 # 启用资源目录
-module "alicloud_resource_manager_resource_directory" {
-  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/resource-directories?ref=1.x"
-  alicloud_resources = var.alicloud_resources
-}
+#module "alicloud_resource_manager_resource_directory" {
+#  source             = "git::https://gitea.home.local/suzhetao/terraform-module-alicloud.git//resource-manager/resource-directories?ref=1.x"
+#  alicloud_resources = var.alicloud_resources
+#}
 
 # 管控策略
 module "alicloud_resource_manager_control_policy" {
